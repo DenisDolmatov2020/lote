06194e42b3cc93e46600be1dce069cf92fce0620
polyarush.d.v <polyarush.d.v@yandex.ru>
2021-08-30 01:17:19 +0700

    asgi

--- lottee_new/asgi.py
+++ lottee_new/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import number.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lottee_new.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),

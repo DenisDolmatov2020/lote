fb89768425ca0b4c166e8fa2938f3cd2a4484661
polyarush.d.v <polyarush.d.v@yandex.ru>
2021-08-30 13:16:11 +0700

    Update asgi.py

--- lottee_new/asgi.py
+++ lottee_new/asgi.py
@@ -11,17 +11,14 @@ application = get_asgi_application()
 
 import os
 import django
-
-django.setup()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lottee_new.settings")
-
+django.setup()
 
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from number import routing
-
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
